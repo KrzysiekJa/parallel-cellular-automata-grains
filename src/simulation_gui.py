a9c8a6cc1d19026386981eab1d2c93629c910e3b
KrzysiekJa <kjmj77@gmail.com>
2022-11-21 21:14:38 +0100

    Added visualization for gui.

--- src/simulation_gui.py
+++ src/simulation_gui.py
@@ -64,8 +64,11 @@ class Ui_MainWindow( QMainWindow ):
         self.iterationsNumberLineEdit.setGeometry(QtCore.QRect(580, 290, 151, 21))
         self.iterationsNumberLineEdit.setObjectName("iterationsNumberLineEdit")
         self.runPushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.runPushButton.setGeometry(QtCore.QRect(600, 320, 113, 32))
+        self.runPushButton.setGeometry(QtCore.QRect(660, 320, 70, 32))
         self.runPushButton.setObjectName("runPushButton")
+        self.dispPushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.dispPushButton.setGeometry(QtCore.QRect(590, 320, 80, 32))
+        self.dispPushButton.setObjectName("dispPushButton")
         self.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(self)
         self.statusbar.setObjectName("statusbar")
@@ -87,6 +90,7 @@ class Ui_MainWindow( QMainWindow ):
         self.iterationsNumberLineEdit.setText(_translate("MainWindow", ""))
         self.iterationsNumberLineEdit.setPlaceholderText("Iterations MC")
         self.runPushButton.setText(_translate("MainWindow", "RUN"))
+        self.dispPushButton.setText(_translate("MainWindow", "Disp Img"))
         
         self.dimensionalityComboBox.addItems(["2D", "3D"])
         self.boundaryConditionsComboBox.addItems(["absorbing", "periodic"])
@@ -94,6 +98,7 @@ class Ui_MainWindow( QMainWindow ):
         self.simulationComboBox.addItems(["none", "monte_carlo"])
 
         self.runPushButton.clicked.connect( self.runPushButtonClicked )
+        self.dispPushButton.clicked.connect( self.loadFunction )
     
     
     def runPushButtonClicked(self):
@@ -134,6 +139,79 @@ class Ui_MainWindow( QMainWindow ):
         tree.write( xml_file, xml_declaration=True )
         
         return xml_file
+    
+    
+    def loadFunction(self):
+        try:
+            df = self.loadFile()
+        except Exception as e:
+            return
+        
+        if (df['z'] == 0).all():
+            self.disp2Dimage(df)
+        else:
+            self.disp3Dimage(df)
+    
+    
+    def loadFile(self):
+        try:
+            filePath, _ = QtWidgets.QFileDialog.getOpenFileName()
+            df = pd.read_csv(filePath, sep=',')
+            df = df[['x', 'y', 'z', 'grainId']]
+        except OSError as err:
+            print("OS error: {0}".format(err))
+            raise
+        
+        return df
+    
+    
+    def disp2Dimage(self, df):
+        image_matrix = self.makePartitionMxt( df, 'x', 'y', df['x'].max()+1 )
+        image_matrix = (image_matrix-1) / (df['grainId'].max()-1) # normalization <0,1>
+        image  = Image.fromarray( np.uint8( cm.gist_earth(image_matrix) * 255 ) )
+        pixmap = QtGui.QPixmap.fromImage( ImageQt(image) )
+        self.labelForPixmap.setPixmap( pixmap.scaled( self.labelForPixmap.size(), QtCore.Qt.KeepAspectRatio ) )
+    
+    
+    def disp3Dimage(self, df):
+        size = df['x'].max() + 1
+        zeros_mtx = np.zeros( (size, size), dtype=int )
+        
+        
+        tmp_mtx_0 = np.flipud( self.makePartitionMxt(df[ df["x"]==0 ], "z", "y", size) ) # buttom
+        image_matrix = np.concatenate( (zeros_mtx, tmp_mtx_0, zeros_mtx), axis=1 )
+        
+        tmp_mtx_0 = np.fliplr( self.makePartitionMxt(df[ df["y"]==0 ], "x", "z", size) )
+        tmp_mtx_1 = self.makePartitionMxt(df[ df["z"]==0 ], "x", "y", size) # front
+        tmp_mtx_2 = self.makePartitionMxt(df[ df["y"]==size-1 ], "x", "z", size)
+        tmp_mtx_row = np.concatenate( (tmp_mtx_0, tmp_mtx_1, tmp_mtx_2), axis=1 )
+        image_matrix = np.concatenate( (image_matrix, tmp_mtx_row), axis=0 )
+        
+        tmp_mtx_0 = self.makePartitionMxt(df[ df["x"]==size-1 ], "z", "y", size) # down
+        tmp_mtx_row = np.concatenate( (zeros_mtx, tmp_mtx_0, zeros_mtx), axis=1 )
+        image_matrix = np.concatenate( (image_matrix, tmp_mtx_row), axis=0 )
+        
+        tmp_mtx_0 = np.flipud( self.makePartitionMxt(df[ df["z"]==size-1 ], "x", "y", size) ) # back
+        tmp_mtx_row = np.concatenate( (zeros_mtx, tmp_mtx_0, zeros_mtx), axis=1 )
+        image_matrix = np.concatenate( (image_matrix, tmp_mtx_row), axis=0 )
+        
+        
+        max_fraction = df['grainId'].max()
+        image_matrix[ image_matrix==0 ] = max_fraction+1
+        image_matrix = (image_matrix-1) / max_fraction # normalization <0,1>
+        
+        image  = Image.fromarray( np.uint8( cm.gist_earth(image_matrix) * 255 ) )
+        pixmap = QtGui.QPixmap.fromImage( ImageQt(image) )
+        self.labelForPixmap.setPixmap( pixmap.scaled( self.labelForPixmap.size(), QtCore.Qt.KeepAspectRatio ) )
+    
+    
+    def makePartitionMxt(self, df, i, j, size):
+        matrix = np.zeros( (size, size), dtype=int )
+        
+        for index, row in df.iterrows():
+            matrix[ row[i] ][ row[j] ] = row['grainId']
+        
+        return matrix
 
 
 
